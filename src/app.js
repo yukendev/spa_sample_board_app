b4a34f46feca6bb569eef1f1d06ff109c2566015
yuken <yuken0906dev@gmail.com>
2022-05-21 12:29:09 +0900

    introduce typescript

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Hello } from './Hello';
 
+console.log('hello~~~~');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log('hello world');
 root.render(<Hello/>);
--- src/server.js
+++ src/server.js
@@ -8,14 +8,16 @@ server.on('request', (req, res) => {
 
   let url = req.url;
   if (url === '/') {
-    fs.readFile('./index.html','UTF-8',(error,data)=>{
+    fs.readFile('./src/index.html','UTF-8',(error,data)=>{
+      console.log("request for '/'");
       res.writeHead(200, {'Content-Type':'text/html'});
       res.write(data);
       res.end();
     });
   } 
   else if (url === '/index.js') {
-    fs.readFile('./index.js','UTF-8',(error,data)=>{
+    fs.readFile('./src/index.js','UTF-8',(error,data)=>{
+      console.log("request for '/index.js'");
       res.writeHead(200, {'Content-Type':'text/plain'});
       res.write(data);
       res.end();
