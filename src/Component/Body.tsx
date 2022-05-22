67f6896f3257f24c05ddfd4643fb3cf3932b0633
yuken <yuken0906dev@gmail.com>
2022-05-22 12:29:35 +0900

    create scrollable component

--- src/Component/Body.tsx
+++ src/Component/Body.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Advertisements } from './Advertisements';
+import { MainContents } from "./MainContents";
 
 export const Body = () => {
   return (
     <div className="h-100 app_body">
       <Advertisements/>
+      <MainContents/>
     </div>
   );
 };
